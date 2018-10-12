7b1b0b216a26ead8cb51dcf82a407cf92aed04c8
David Llop <d.lloople@gmail.com>
2018-10-12 10:57:59 +0200

    Start integration with GitHub

---
+++ app/Services/GitHub/GitHubCommit.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Deployer\Services\GitHub;
+
+use Deployer\Services\Commit;
+
+class GitHubCommit extends Commit
+{
+
+    public function __construct($commit, $branch)
+    {
+        parent::__construct($commit);
+
+        $this->setAuthor($commit->author->name);
+        $this->setType('branch');
+        $this->setMessage($commit->message);
+        $this->setBranch($branch);
+
+        return $this;
+    }
+}
