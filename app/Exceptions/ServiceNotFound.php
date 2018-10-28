2f720c9d4742d1ab582e72dd9b75a65d84a8a7fb
David Llop <d.lloople@gmail.com>
2018-10-28 14:27:50 +0100

    Improve error message when service class is not found

--- app/Exceptions/ServiceNotFound.php
+++ app/Exceptions/ServiceNotFound.php
@@ -9,8 +9,7 @@ class ServiceNotFound extends \Exception
 
     public function __construct($class, $message = "", $code = 0, Throwable $previous = null)
     {
-        $code = 403;
-        parent::__construct($message, $code, $previous);
+        parent::__construct("Service {$class} was not found. Check your repositories configuration", 403, $previous);
     }
 
 }
